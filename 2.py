77408295235451d6d5eebee707f4cbbbe881ac44
ray05131111 <214211@clvsc.tyc.edu.tw>
2025-02-13 14:49:42 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+
+# 設定 LINE Channel Access Token 和 Secret
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+@app.route('/')
+def home():
+    return "LINE BOT 首頁"
